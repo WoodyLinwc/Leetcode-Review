b5a36eeafe963ce2712a0d240c421b2075e9d284
Woody Lin <wancheng.lin001@umb.edu>
2025-03-04 16:43:54 -0500

    Update top_k_frequent_elements.js

--- solutions/medium/top_k_frequent_elements.js
+++ solutions/medium/top_k_frequent_elements.js
@@ -14,8 +14,24 @@
 /**
  * Your solution goes here
  */
+var topKFrequent = function(nums, k) {
+    //count the occurrence 
+    let map = new Map();
+    for(let n of nums){
+        map.set(n, (map.get(n) || 0) + 1);
+    }
 
-// Time Complexity: 
-// Space Complexity: 
+    //convert map to array and sort it based on freq in descending order
+    let sortArray = [...map.entries()].sort((a,b) => b[1] - a[1]);
+
+    // output elements to res
+    let res = [];
+    for(let i = 0; i < k; i++){
+        res.push(sortArray[i][0]);
+    }
+    return res;
+};
+// Time Complexity: O(nlogn) sorting
+// Space Complexity: O(n)
 
 // Test cases
