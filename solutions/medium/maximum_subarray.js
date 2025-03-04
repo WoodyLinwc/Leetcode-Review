e5c34e6e3612a26f15e2ae1d93abf72fb0418de0
Woody Lin <wancheng.lin001@umb.edu>
2025-03-04 16:47:49 -0500

    Update maximum_subarray.js

--- solutions/medium/maximum_subarray.js
+++ solutions/medium/maximum_subarray.js
@@ -14,6 +14,32 @@
 /**
  * Your solution goes here
  */
+var maxSubArray = function(nums) {
+    let sum = 0;
+    let max = nums[0];
+
+    // let start = 0, end = 0, tempStart = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i];
+
+        if(sum > max){
+            max = sum;
+            // start = tempStart;
+            // end = i;
+        }
+
+        if(sum < 0){
+            sum = 0;
+            // tempStart = i + 1;
+        }
+    }
+    return max;
+    // return {
+    //     max: max,
+    //     array: nums.slice(start, end+1)
+    // };
+};
 
 // Time Complexity: 
 // Space Complexity: 
